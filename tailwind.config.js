2a3681942f24d07b8e19fb6d65c271308fdb5801
sapamaunet-cpu <sapamau.net@gmail.com>
2026-02-21 05:07:18 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,9 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}", // Tambahkan ini jika ada style di lib
   ],
   theme: {
-    extend: {
-      colors: {
-        // Anda bisa menambah warna custom netflix di sini jika mau
-        brand: '#E50914',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
-
