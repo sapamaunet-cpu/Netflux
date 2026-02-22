ff4d043ae42ab3824bd76c5c2687d3e3563cd964
sapamaunet-cpu <sapamau.net@gmail.com>
2026-02-22 12:21:59 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import { getMovies } from '../lib/tmdb';
 import MovieList from '../components/MovieList';
 
 export default async function Home() {
-  const data = await getMovies('/movie/popular', 1);
+  const data = await getMovies('/movie/now_playing', 1);
   return (
     <div className="max-w-7xl mx-auto py-8">
       <h2 className="text-2xl font-bold px-4 mb-8 flex items-center gap-2">
-        <span className="w-1 h-8 bg-red-600 rounded-full"></span> Trending Sekarang
+        <span className="w-1 h-8 bg-red-600 rounded-full"></span> Film Baru
       </h2>
       <MovieList initialData={data} endpoint="/movie/popular" limit={100} />
     </div>
