a5cd19582f18fd35f0926d4df993f3597a729232
sapamaunet-cpu <sapamau.net@gmail.com>
2026-02-24 00:24:05 +0700

    Update page.js

--- app/search/page.js
+++ app/search/page.js
@@ -1,6 +1,8 @@
 import { getMovieDetail } from '../../lib/tmdb';
 import MovieList from '../../components/MovieList';
 
+export const dynamic = 'force-dynamic';
+
 export default async function SearchPage({ searchParams }) {
   // Mengambil kata kunci dari URL (?q=nama-film)
   const query = searchParams.q || '';
