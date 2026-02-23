2e5f24da76077fffd69ccea09811b513b4c48d5a
sapamaunet-cpu <sapamau.net@gmail.com>
2026-02-24 01:01:25 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import { getMovieDetail } from '../lib/tmdb';
+import { getMovies } from '../lib/tmdb';
 import MovieList from '../components/MovieList';
 
 export default async function Home() {
-  const data = await getMovieDetail('/movie/now_playing', 1);
+  const data = await getMovies('/movie/now_playing', 1);
   return (
     <div className="max-w-7xl mx-auto py-8">
       <h2 className="text-2xl font-bold px-4 mb-8 flex items-center gap-2">
