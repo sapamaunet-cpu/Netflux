c2254f19904c4139c54ba59fc8a17b611db5f6ed
sapamaunet-cpu <sapamau.net@gmail.com>
2026-03-09 03:40:25 +0700

    Update page.js

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -37,12 +37,7 @@ export default async function MoviePage({ params, searchParams }) {
         <div className="flex-1"> {/* Ini flex-1 utama */}
           {/* PLAYER */}
           {isPlaying && (
-            <div id="player" className="mb-10">
-              <div className="aspect-video bg-zinc-900 rounded-2xl overflow-hidden border border-zinc-800 shadow-2xl">
-          {/* <iframe src={`https://vidsrc.me/embed/movie?tmdb=${movie.id}`} className="w-full h-full" allowFullScreen /> */}
             <MoviePlayer movieId={movie.id} />
-              </div>
-            </div>
           )}
 
           {/* SINOPSIS & INFO */}
