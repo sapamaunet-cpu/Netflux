fd77787abdd7003fea9cceb84724725da4f1cf1e
sapamaunet-cpu <sapamau.net@gmail.com>
2026-03-11 01:53:20 +0700

    Update page.js

--- app/dewasa/page.js
+++ app/dewasa/page.js
@@ -2,13 +2,13 @@ import { getMovies } from '../../lib/tmdb';
 import MovieList from '../../components/MovieList';
 
 export default async function Home() {
-  const data = await getMovies('/discover/movie?with_keywords=9748,155461,159551&sort_by=popularity.desc', 1);
+  const data = await getMovies('/discover/movie?with_keywords=9748|155461|159551&sort_by=popularity.desc', 1);
   return (
     <div className="max-w-7xl mx-auto py-8">
       <h2 className="text-2xl font-bold px-4 mb-8 flex items-center gap-2">
         <span className="w-1 h-8 bg-red-600 rounded-full"></span> Film Baru
       </h2>
-      <MovieList initialData={data} endpoint="/discover/movie?with_keywords=9748,155461,159551&sort_by=popularity.desc" limit={100} />
+      <MovieList initialData={data} endpoint="/discover/movie?with_keywords=9748|155461|159551&sort_by=popularity.desc" limit={100} />
     </div>
   );
 }
